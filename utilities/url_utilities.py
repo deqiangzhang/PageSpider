019bb144cb4bb073e6e5440766751384881a0c64
zhangdq <zhangdq@gmail.com>
2019-04-10 16:33:56 -0700

    Initial commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,12 @@
+def load_urls_from_file(file_path: str):
+    #TODO: add code needed to read the text file with urls in it
+    pass
+
+def load_page(url: str):
+    #TODO: add the code to read the url
+    pass
+
+def scrape_page(page_content: str):
+    #TODO: analyse the text
+    pass
+
